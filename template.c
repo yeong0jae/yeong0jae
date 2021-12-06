3905ecf8471fca9c72760a70d03f62d64b772b69
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2021-12-06 19:48:35 +0900

    Create template.c

---
+++ template.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	int T, i, j, n;
+	
+	scanf("%d", T);
+	
+	for(i = 0; i < T; i++;) {
+		scanf("%d %d", x, y);
+		
+		d = y - x;
+		
+		if(d == 1)
+			printf("%d\n", d);
+		else if(d == 2)
+			printf("%d\n", d);
+		else {
+			for(j = 3; j <= d; j++) {
+				
+			}
+		}
+	}
+	
+	
+
+	return 0;
+}
