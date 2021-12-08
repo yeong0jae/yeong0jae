76e58ec4602b371becb237f4055f584a40d813c9
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2021-12-08 23:59:11 +0900

    Update template.c

--- template.c
+++ template.c
@@ -26,3 +26,38 @@ int main(void) {
 
 	return 0;
 }
+
+
+#include <stdio.h>
+
+int main(void) {
+
+	int a, b, i, j, sum = 0, m, cnt = 0;
+	
+	scanf("%d", &a);
+	scanf("%d", &b);
+	
+	for(i = a; i <= b; i++) {
+		for(j = 1; j < i; j++) {
+			if(i % j == 0)
+				break;
+		}
+		if(j == i && cnt = 0) {
+			sum += i;
+			m = i;
+			cnt = 1;
+		}
+		if(j == i && cnt = 1) {
+			sum += i;
+			if(m > i)
+				m = i;
+		}
+	}
+	
+	printf("%d\n", sum);
+	printf("%d", m);
+
+
+
+	return 0;
+}
