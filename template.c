0a8848df190050f972bae5c5e86f07de847d9eb2
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2021-12-12 23:58:27 +0900

    Update template.c

--- template.c
+++ template.c
@@ -43,3 +43,59 @@ int main(void) {
 	
 	return 0;
 }
+
+#include <stdio.h>
+
+int search(int a[], int n, int key) {
+	int i = 0;
+	while(1){
+		if(i == n)
+			return -1;
+		if(a[i] == key)
+			return 1;
+		i++;
+	}
+}
+
+int main(void) {
+	
+	int T, pn[10001], p[2000], k = 0, n, a[2]; //왜 10000까지 해야되지?? 5000까지 가능
+	
+	scanf("%d", &T);
+	
+	for(int i = 2; i <= 10000; i++) {
+		pn[i] = 1;
+	}
+	
+	for(int i = 2; i <= 10000; i++) {
+		if(pn[i] == 0) continue;
+		for(int j = i + i; j <= 10000; j += i) {
+			pn[j] = 0;
+		}
+	}
+	
+	for(int i = 2; i <= 10000; i++) {
+		if(pn[i] == 1) {
+			p[k] = i;
+			k++;
+		}
+	}
+	
+	
+	for(int i = 0; i < T; i++) {
+		scanf("%d", &n);
+		int l = 0;
+		
+		while(p[l] <= n / 2) {
+			if(search(p, 2000, n - p[l]) == 1) {
+				a[0] = p[l];
+				a[1] = n - p[l];
+			}
+			l++;
+		}
+		
+		printf("%d %d\n", a[0], a[1]);
+	}
+
+	return 0;
+}
