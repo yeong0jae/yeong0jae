656132ec1d3e6e42f822ba4acf9a4f97567de36d
kyj91032 <83624600+kyj91032@users.noreply.github.com>
2022-01-06 23:45:03 +0900

    Update Template.c

--- Template.c
+++ Template.c
@@ -14,20 +14,29 @@ int main(void) {
 	return 0;
 }
 
-#include <stdio.h> //선형검색, 이분탐색(정렬 시) 공부해야됨.
 
-int search(int a[], int n, int key) {
-	int i = 0;
-	while(1){
-		if(i == n)
-			return -1;
-		if(a[i] == key)
-			return 1;
-		i++;
-	}
-}
+N, M = map(int, input().split())
+
+graph = []
+for i in range(N):
+	graph.append(list(map(int, input())))
+
+def dfs(x, y):
+	if x <= -1 or x >= n or y <= -1 or y >= m:
+		return False
+	if graph[x][y] == 0:
+		graph[x][y] = 1
+		dfs(x - 1, y)
+		dfs(x, y - 1)
+		dfs(x + 1, y)
+		dfs(x, y + 1)
+		return True
+	return False
 
 
-n, m = map(int, input().split())
-a, b, c = map(int, input().split())
-map = [list(map(int, input().split())) for i in range(n)]
+result = 0
+for i in range(n):
+	for j in range(m):
+		if dfs(i, j) == True:
+			result += 1
+print(result)
